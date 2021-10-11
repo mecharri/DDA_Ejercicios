78385a1744417dcd81354ebd18b7d6fa138dd0c7
MartinE <echarri.martin@gmail.com>
2021-10-11 11:43:43 -0300

    Commit para poder volver a atras y hacer consultas

--- GP01/Ej_1/ej_1.v
+++ GP01/Ej_1/ej_1.v
@@ -7,12 +7,13 @@
 `timescale 1ns/100ps
  
 module top_ej_1(
-    output  [5:0]   o_data,
-    output          o_overflow,
-    input   [2:0]   i_data1,
-    input   [2:0]   i_data2,
-    input           i_rst_n,
-    input   [1:0]   i_sel,
+    //Puertos comentados para usarlos vía el VIO
+//    output  [5:0]   o_data,
+//    output          o_overflow,
+//    input   [2:0]   i_data1,
+//   input   [2:0]   i_data2,
+//    input           i_rst_n,
+//    input   [1:0]   i_sel,
     input           clock
 );//! Toplevel del Ej1
 
@@ -21,6 +22,16 @@ module top_ej_1(
 wire    [3:0]   w_sumador_realimentador;    //! Wire que conecta el sumador con el realimentador
 wire    [5:0]   w_o_data;                   //! Wire que conecta la salida del realimentador a la salida del toplevel
 wire            w_o_overflow;               //! Wire que conecta la salida del sumador con la salida del toplevel
+
+//Cables para conectar el VIO con el toplevel
+//Comentar si se simula sin el VIO
+wire    [5:0]   o_data;
+wire            o_overflow;
+wire    [2:0]   i_data1;
+wire    [2:0]   i_data2;
+wire            i_rst_n;
+wire    [1:0]   i_sel;
+
 
 //! Instacio un bloque realimentador
 realimentador 
@@ -43,6 +54,20 @@ sumador_selectivo
         .i_data1(i_data1),
         .i_data2(i_data2),
         .i_sel(i_sel)
+    );
+
+//! Instancio el VIO
+//Comentar si se simula sin el VIO
+ vio
+    u_vio
+    (
+        .clk_0          (clock),
+        .probe_in0_0    (o_data),
+        .probe_in1_0    (o_overflow),
+        .probe_out0_0   (i_data1),
+        .probe_out1_0   (i_data2),
+        .probe_out2_0   (i_sel),
+        .probe_out3_0   (i_rst_n)
     );
 
 //! Asigno las salidas de los modulos a la salida del toplevel
