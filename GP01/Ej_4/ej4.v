9b14a31ff39714588ddc00eaa6b8f0f51a141308
Martin Echarri <echarri.martin+gh@gmail.com>
2021-10-19 18:14:45 -0300

    Tocando el ejercicio 4 para ver que le pasa. Se agrega el gitignore

--- GP01/Ej_4/ej4.v
+++ GP01/Ej_4/ej4.v
@@ -14,12 +14,14 @@ module top_ej_4
     input                        clock
 );
 
-reg     [NB_DATA-1 : 0]      r_xm1;  //! x[n-1]
-reg     [NB_DATA-1 : 0]      r_xm2;  //! x[n-2]
-reg     [NB_DATA-1 : 0]      r_xm3;  //! x[n-3]
-reg     [NB_DATA-1 : 0]      r_y;    //! y[n]
-reg     [NB_DATA-1 : 0]      r_ym1;  //! y[n-1]
-reg     [NB_DATA-1 : 0]      r_ym2;  //! y[n-2]
+reg     [NB_DATA-1 : 0]      r_xm1;             //! x[n-1]
+reg     [NB_DATA-1 : 0]      r_xm2;             //! x[n-2]
+reg     [NB_DATA-1 : 0]      r_xm3;             //! x[n-3]
+reg     [NB_DATA-1 : 0]      r_y;               //! y[n]
+reg     [NB_DATA-1 : 0]      r_ym1;             //! y[n-1]
+reg     [NB_DATA-1 : 0]      r_ym1_shifted;     //! y[n-1]*0,5
+reg     [NB_DATA-1 : 0]      r_ym2;             //! y[n-2]*0,25
+reg     [NB_DATA-1 : 0]      r_ym2_shifted;     //! y[n-1]
 
 always  @(posedge clock or negedge i_rst_n )
 begin
@@ -33,16 +35,22 @@ begin
         r_ym2   <=  0;
     end
     else begin
-        //! Actualizo el historial de las entradas
+
+        //! Hago los shift
+        r_ym1_shifted = r_ym1 >>> 1;
+        r_ym2_shifted = r_ym2 >>> 2;
+        //! Hago la cuenta
+        r_y     <=  i_x + r_ym1_shifted + r_ym2_shifted- r_xm1 + r_xm2 + r_xm3 ;
+        //! y   =   x[n] - x[n-1] + x[n-2] + x[n-3] + 0,5*y[n-1] + 0,25*y[n-2]
+
+       //! Actualizo el historial de las entradas
         r_xm3   <=  r_xm2;
         r_xm2   <=  r_xm1;
         r_xm1   <=  i_x;
+        
         //! Actualizo el historial de las salidas
         r_ym2   <=  r_ym1;
         r_ym1   <=  r_y;
-        //! Hago la cuenta
-        r_y     <=  i_x - r_xm1 + r_xm2 + r_xm3 + (r_ym1 >>> 1) + (r_ym2 >>> 2);
-        //! y   =   x[n] - x[n-1] + x[n-2] + x[n-3] + 0,5*y[n-1] + 0,25*y[n-2]
     end
 
 end 
